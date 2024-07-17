3938cf0b77aeb139862d438e86fa00c1fdefec27
yrenqm <eyquiliche@gmail.com>
2024-07-17 12:56:10 -0500

    Creacion del backend y cronograma

---
+++ thullo_backend/src/main/java/com/yrenqm/devchallenges/thullobackend/ThulloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yrenqm.devchallenges.thullobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThulloBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThulloBackendApplication.class, args);
+	}
+
+}
---
+++ thullo_backend/src/test/java/com/yrenqm/devchallenges/thullobackend/ThulloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yrenqm.devchallenges.thullobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThulloBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
